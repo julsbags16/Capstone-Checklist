c2c806c52f5bbaf6577f2b8fbb7f05b003611a29
julsbags16 <165006951+julsbags16@users.noreply.github.com>
2026-01-14 21:14:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://uypjfxlkcqukczskdwmt.supabase.co';
-const SUPABASE_KEY = 'process.env.SUPABASE_KEY';
+const SUPABASE_KEY = 'sb_publishable_r8TrGXZdB56xhhTjukNZ8g_bxYY2o8U';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // --- AUTH LOGIC ---
@@ -81,4 +81,5 @@ function renderTask(task) {
 // Initialize
 
 checkUser();
+
 
